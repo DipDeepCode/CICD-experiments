c8ef616293147dfea034b4e83e38dbc0366dc9f0
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-16 21:23:20 +0300

    feat :: init commit

---
+++ src/test/java/ru/ddc/exp_01/Exp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.exp_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

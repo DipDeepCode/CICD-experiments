b353ce56610b88c158762b9cc7e992d9a29cf209
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-16 21:52:31 +0300

    feat :: test cicd checkout, set up JDK, checkstyle and build with maven

--- src/main/java/ru/ddc/exp_01/Exp01Application.java
+++ src/main/java/ru/ddc/exp_01/Exp01Application.java
@@ -3,6 +3,7 @@ package ru.ddc.exp_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("java:S1118")
 @SpringBootApplication
 public class Exp01Application {
 
@@ -11,7 +12,7 @@ public class Exp01Application {
      *
      * @param args the arguments from cli
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Exp01Application.class, args);
     }
 
